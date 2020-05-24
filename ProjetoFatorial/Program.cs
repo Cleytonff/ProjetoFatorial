07d9aa2048da48ab43e829e6cd032dc7d3cff314
Cleyton Ferreira <cleytonferreirafranco@Cleyton-MacBook.local>
2020-05-24 12:57:58 -0300

    Primeiro commit da versão ProjetoFatorial

---
+++ ProjetoFatorial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ProjetoFatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Projeto Fatorial!");
+            Console.WriteLine(CalcularFatorial(5));
+        }
+
+        public static int CalcularFatorial(int n){
+            int resultado = 1;
+            for (int i = 2; i <= n; i++){
+                resultado *= i;
+
+
+            }
+            return resultado;
+        }
+    }
+}
